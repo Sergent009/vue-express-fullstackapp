362352eb8bd7d33bba34e243fe40133eaab42a30
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-03-03 16:50:07 +0500

    another one component is splitted into two more components

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+<div id="page-wrap">
+  <ProductsListItem v-for="product in cartItems" :key="product.id" :product="product" />
+</div>
+</div>
+</template>
+
+<script>
+import ProductsListItem from './ProductsListItem.vue'
+export default {
+    name: 'ProductsList',
+    props:['cartItems'],
+    components:{
+        ProductsListItem
+    },
+}
+</script>
+
+<style>
+
+.page-wrap{
+    margin: 20px;
+    padding: 20px;
+    background-color: #f0f0f0;
+    border: 1px solid #ddd;
+}
+
+</style>
---
+++ src/components/ProductsListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="product-container">
+    <img :src="product.imageUrl" class="product-image">
+    <div class="details-wrap">
+      <h3>{{product.name}}</h3>
+      <p>${{product.price}}</p>
+    </div>
+    <button class="remove-button">Remove From Cart</button>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'ProductsListItem',
+    props: ['product'],
+}
+</script>
+
+<style>
+
+  .product-image {
+    flex: 1;
+    height: 100px;
+    max-width: 100px;
+  }
+
+  .details-wrap {
+    padding: 0 16px;
+    flex: 3;
+  }
+
+  .remove-button {
+    flex: 1;
+    margin: auto;
+  }
+
+  .product-container {
+    align-content: 'center';
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    padding: 16px;
+    width: 100%;
+  }
+
+</style>
